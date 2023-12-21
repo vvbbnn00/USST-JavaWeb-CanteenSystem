97b375677043670284fdfbc2608a7c10fa6e9287
Bluerkangkang <2636207104@qq.com>
2023-12-21 17:08:33 +0800

    菜品编辑/新增,评论详情查看

--- AdminFrontend/src/api/item.ts
+++ AdminFrontend/src/api/item.ts
@@ -15,3 +15,39 @@ export const deleteItem = (itemId: number) => {
         method: 'DELETE'
     });
 }
+
+export const createItem = (form: any,canteenId: number) => {
+    return request({
+        url: `${BASE_URL}/api/rest/canteen/${canteenId}/cuisine/${form.cuisineId}/item`,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify(form)
+    });
+}
+
+export const updateItem = (form: any) => {
+    return request({
+        url: `${BASE_URL}/api/rest/canteen/item/${form.itemId}`,
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify(form)
+    });
+}
+
+export const getItemComment = (itemId: number) => {
+    return request({
+        url: `${BASE_URL}/api/rest/canteen/item/${itemId}/comment`,
+        method: 'GET'
+    });
+}
+
+export const deleteItemComment = (itemId: number,commentId: number) => {
+    return request({
+        url: `${BASE_URL}/api/rest/canteen/item/${itemId}/comment/${commentId}`,
+        method: 'DELETE'
+    });
+}
