35a84b699bd140b4eca393094605ef4af29d90a2
vvbbnn00 <vvbbnn00@foxmail.com>
2023-12-22 16:18:42 +0800

    在PublishForm中引入文件选择和表单增强
    此次提交在PublishForm组件中引入了新的功能。它添加了一个用于选择和上传多个文件的机制，并根据预定义的最大值限制数量。同时，它还通过添加标题的输入字段和为删除选定文件的细致控制来增强表单

--- CommunityFrontend/src/components/community/PublishForm.js
+++ CommunityFrontend/src/components/community/PublishForm.js
@@ -1,7 +1,41 @@
-import {Image, Button, Textarea} from "@nextui-org/react";
+import {Image, Button, Textarea, Input} from "@nextui-org/react";
 import {CameraIcon} from "@/components/icons/CameraIcon";
+import {useState} from "react";
+import {CloseCircleOutlined, CloseOutlined} from "@ant-design/icons";
+
+
+const selectFile = (maxFileCount) => {
+    const SUPPORT_FILE_TYPES = ["image/png", "image/jpeg", "image/gif", "image/webp"];
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.multiple = true;
+    fileInput.accept = SUPPORT_FILE_TYPES.join(",");
+    fileInput.click();
+    return new Promise((resolve, reject) => {
+        fileInput.onchange = (e) => {
+            const files = e.target.files;
+            if (files.length > maxFileCount) {
+                reject("文件数量超过上限");
+            }
+            console.log(files);
+            resolve(Array.from(files));
+        }
+    })
+}
+
 
 export default function PublishForm({user}) {
+    const [files, setFiles] = useState([]);
+
+    const addFiles = async () => {
+        try {
+            const files = await selectFile(9);
+            setFiles(files);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <div className={"bg-white rounded-md p-5 w-full shadow-xl"}>
             <div className={"flex flex-row"}>
@@ -10,14 +44,23 @@ export default function PublishForm({user}) {
                 </div>
                 <div className={"grow"}>
                     <div className={"mb-3"}>
+                        <Input
+                            name={"title"}
+                            placeholder={"取一个标题吧"}
+                            className={"w-full mb-2"}
+                            size={"sm"}
+                        />
                         <Textarea
+                            name={"content"}
                             className={"w-full"}
                             placeholder={"有什么新鲜事想告诉大家？"}
                         />
                     </div>
                     <div className={"flex flex-row justify-between pl-1 pr-1"}>
                         <div
-                            className={"hover:bg-blue-100 rounded-full p-2 text-blue-700 cursor-pointer transition-all"}>
+                            className={"hover:bg-blue-100 rounded-full p-2 text-blue-700 cursor-pointer transition-all"}
+                            onClick={addFiles}
+                        >
                             <CameraIcon/>
                         </div>
                         <div>
@@ -26,6 +69,22 @@ export default function PublishForm({user}) {
                                 className={"w-full rounded-full"}
                             >发布</Button>
                         </div>
+                    </div>
+                    <div className={"flex flex-row gap-2 pt-2"}>
+                        {files?.map((file, index) => {
+                            return <div key={index} className={"relative"}>
+                                <Image src={URL.createObjectURL(file)} width={"150px"}
+                                       className={"rounded-md object-cover w-[150px] h-[150px]"}/>
+                                <div
+                                    className={"absolute text-sm top-0 right-0 text-white rounded-r-md p-1.5 cursor-pointer z-10 bg-[rgba(0,0,0,0.5)]"}
+                                    onClick={() => {
+                                        setFiles(files.filter((_, i) => i !== index));
+                                    }}
+                                >
+                                    <CloseOutlined/>
+                                </div>
+                            </div>
+                        })}
                     </div>
                 </div>
             </div>
